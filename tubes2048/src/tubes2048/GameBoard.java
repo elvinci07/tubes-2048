8b538cfa264975265bd4b0733a6eb2d37f16ef47
Yobel <yobel.sinaga07@gmail.com>
2026-05-24 20:11:50 +0700

    inisialisasi awal GameBoard.java

---
+++ tubes2048/src/tubes2048/GameBoard.java
@@ -0,0 +1,50 @@
+package tubes2048;
+
+import java.util.ArrayList;
+
+public class GameBoard {
+    private Tile[][] board;
+    private int score;
+
+    // Constructor
+    public GameBoard() {
+        board = new Tile[4][4];
+        initializeBoard();
+    }
+
+    // 1. Inisialisasi awal papan
+    public void initializeBoard() {
+        score = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                board[i][j] = new Tile(0); // Isi dengan nilai 0 (kosong)
+            }
+        }
+        // Munculkan 2 angka di awal
+        spawnTile();
+        spawnTile();
+    }
+
+    // 2. Memunculkan angka 2 atau 4 di posisi acak yang kosong
+    public void spawnTile() {
+        ArrayList<int[]> emptyCells = new ArrayList<>();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (board[i][j].getValue() == 0) {
+                    emptyCells.add(new int[]{i, j});
+                }
+            }
+        }
+
+        if (emptyCells.isEmpty()) return;
+
+        // Pilih indeks acak
+        int randomIndex = (int) (Math.random() * emptyCells.size());
+        int[] chosenCell = emptyCells.get(randomIndex);
+
+        // Probabilitas: 90% angka 2, 10% angka 4
+        int newNumber = (Math.random() < 0.9) ? 2 : 4;
+        board[chosenCell[0]][chosenCell[1]].setValue(newNumber);
+    }
+}
