9bf89706e57feeb2432612d5251485e827d96f7a
Yobel <yobel.sinaga07@gmail.com>
2026-05-24 19:52:19 +0700

    membuat class Tile

---
+++ tubes2048/src/tubes2048/Tile.java
@@ -0,0 +1,16 @@
+package tubes2048;
+
+public class Tile {
+    private int value;
+
+    public Tile(int value) {
+        this.value = value;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
